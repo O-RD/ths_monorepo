d9a84007c5bed39380c66684705c586b352b3f29
roshgregory <roshangnoronha@gmail.com>
2022-09-27 17:46:47 +0530

    working

--- flag_handler.go
+++ flag_handler.go
@@ -7,7 +7,7 @@ import (
 func process_flags() (string, string, int) {
 
 	var port = *flag.String("p", "41803", "channel id for connecting")
-	var version = *flag.String("t", "t1", "Type of run")
+	var version = *flag.String("t", "t1", "Type of run") //
 	var threshold = *flag.Int("thresh", 2, "Type of run")
 
 	return port, version, threshold
