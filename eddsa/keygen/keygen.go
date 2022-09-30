26c1a862e3789b9bde3eda643e5968d9ce40d25f
roshgregory <roshangnoronha@gmail.com>
2022-09-30 15:31:20 +0530

    p2p_keygen

--- eddsa/keygen/keygen.go
+++ eddsa/keygen/keygen.go
@@ -2,19 +2,30 @@ package keygen
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/O-RD/ths_monorepo/p2p"
 	"github.com/O-RD/ths_monorepo/ths"
 )
 
-func Start(send_chan chan ths.Message, p ths.P2P) {
+func Start(send_chan chan ths.Message, p *ths.P2P, receive_chan chan ths.Payload) {
+
+	p2p.Input_Stream_listener(p, receive_chan)
 
 	go p2p.Send(send_chan)
+	time.Sleep(time.Second * 2)
 	for i := 0; i < len(p.Peers); i++ {
-		fmt.Println(p.Peers[i].Id)
-		send_chan <- ths.Message{From: p.Host.ID(),
+
+		send_chan <- ths.Message{From: *p,
+			Type:    2,
 			To:      p.Peers[i].Id,
-			Payload: "Test"}
+			Payload: "Test",
+			End:     0}
 
 	}
+
+	for {
+		fmt.Println(<-receive_chan)
+	}
+
 }
