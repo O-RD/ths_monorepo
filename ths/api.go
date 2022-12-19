63bb7dcd16e1e41703255b5e2dc3750a28c48094
pratikshapthfdr <pratiksha@zebpay.com>
2022-12-19 16:53:25 +0530

    key group api function request added for get and post request api calls

--- ths/api.go
+++ ths/api.go
@@ -10,7 +10,7 @@ import (
 )
 
 func api_get(send_topic string) []postgres {
-
+    
 	resp, err := http.Get("http://43.205.198.157:3000/get_peers/" + send_topic)
 	if err != nil {
 		log.Fatalln(err)
@@ -34,7 +34,9 @@ type postgres struct {
 	Item string   `json:"topic"`
 	Ip   []string `json:"ip"`
 	Peer string   `json:"peer"`
-}
+} 
+
+
 
 func api_send(p *P2P) {
 	url := "http://43.205.198.157:3000/advertise/"
@@ -48,6 +50,56 @@ func api_send(p *P2P) {
 	responseBody := bytes.NewBuffer(postBody)
 	//Leverage Go's HTTP Post function to make request
 	resp, err := http.Post("http://43.205.198.157:3000/advertise", "application/json", responseBody)
+	//Handle Error
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	fmt.Println(resp.StatusCode)
+}
+
+type group_sign_key struct {
+	Group_pub_key string `json:"group_pub_key"`
+	Partial_key   string `json:"partial_key"`
+	Id            string `json:"id"`
+	Sign          string `json:"sign"`
+	N             int    `json:"n"`
+	T             int    `json:"t"`
+}
+
+
+func api_get_group_request(group_pub_key string) (random_message string){
+    
+	resp, err := http.Get("localhost:3000/request_to_save_group/" + group_pub_key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// fmt.Println(resp)
+	if resp.StatusCode == http.StatusOK {
+		//bodyBytes, err := io.ReadAll(resp.Body)
+	    return random_message
+	}
+	return 
+}
+
+
+func api_post_group_request(key *group_sign_key) {
+  //func api_post_group_request(p *P2P) {
+    
+	url := "localhost:3000/save_group_key"
+	fmt.Println("URL:>", url)
+
+	postBody, _ := json.Marshal(group_sign_key{ 
+        Group_pub_key : key.Group_pub_key , 
+	    Partial_key   : key.Partial_key,        
+	    Id            : key.Id  ,
+	    Sign          : key.Sign ,
+	    N             : key.N  ,
+	    T             : key.T ,
+		// ?
+	})
+	responseBody := bytes.NewBuffer(postBody)
+	//Leverage Go's HTTP Post function to make request
+	resp, err := http.Post("localhost:3000/save_group_key", "application/json", responseBody)
 	//Handle Error
 	if err != nil {
 		log.Fatalf("An Error Occured %v", err)
