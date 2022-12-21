b8cf2b0f5b481ce4fb13e8398815d889cc6d4ba6
pratikshapthfdr <pratiksha@zebpay.com>
2022-12-21 11:40:50 +0530

    api functions added

--- ths/api.go
+++ ths/api.go
@@ -105,4 +105,151 @@ func api_post_group_request(key *group_sign_key) {
 		log.Fatalf("An Error Occured %v", err)
 	}
 	fmt.Println(resp.StatusCode)
+} 
+
+type group_random_data struct {
+	Group_pub_key string   `json:"group_pub_key"`
+	Partial_key   string   `json:"partial_key"`
+	Id            string   `json:"id"`
+	Sign          string   `json:"sign"`
+	N             int      `json:"n"`
+	T             int      `json:"t"`
+	Random_tag    []string `json:"random_tag"`
+	Random_val    []string `json:"random_val"`
+} 
+
+func get_request_to_save_random(group_pub_key string) (random_message string){
+    
+	resp, err := http.Get("localhost:3000/request_to_save_random/" + group_pub_key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// fmt.Println(resp)
+	if resp.StatusCode == http.StatusOK {
+		//bodyBytes, err := io.ReadAll(resp.Body)
+	    return random_message
+	}
+	return 
 }
+
+func post_save_random_values( key *group_random_data){ 
+	
+	url := "localhost:3000/save_random_values"
+	fmt.Println("URL:>", url)
+
+	postBody, _ := json.Marshal(group_random_data{ 
+        Group_pub_key : key.Group_pub_key , 
+	    Partial_key   : key.Partial_key,        
+	    Id            : key.Id  ,
+	    Sign          : key.Sign ,
+	    N             : key.N  ,
+	    T             : key.T ,
+		Random_tag    : key.Random_tag ,
+		Random_val    : key.Random_val,
+	
+	})
+	responseBody := bytes.NewBuffer(postBody)
+	//Leverage Go's HTTP Post function to make request
+	resp, err := http.Post("localhost:3000/save_random_values", "application/json", responseBody)
+	//Handle Error
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	fmt.Println(resp.StatusCode)
+	
+} 
+
+
+
+func get_Get_signatures(group_pub_key string) (random_message string){
+    
+	resp, err := http.Get("localhost:3000/get_signatures/" + group_pub_key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// fmt.Println(resp)
+	if resp.StatusCode == http.StatusOK {
+		//bodyBytes, err := io.ReadAll(resp.Body)
+	    return random_message
+	}
+	return 
+}
+
+func get_all_signatures(group_pub_key string) (random_message string){
+    
+	resp, err := http.Get("localhost:3000/request_to_save_random/" + group_pub_key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// fmt.Println(resp)
+	if resp.StatusCode == http.StatusOK {
+		//bodyBytes, err := io.ReadAll(resp.Body)
+	    return random_message
+	}
+	return 
+}
+/*
+/sign_message POST
+Input:
+{
+ “group_pub key”:    type String,
+"Partial_key" : type String
+ “id”:  peer_id ,    type strings
+"txn_message":string
+ "sign": string
+"random_tag" string.
+"nonce":int
+
+}
+Output: 200
+
+*/
+func post_Post_signatures(p *P2P) {
+	url := "localhost:3000/post_signature"
+	fmt.Println("URL:>", url)
+
+	postBody, _ := json.Marshal(postgres{
+		Item: p.Topic,
+		Ip:   p.Host_ips,
+		Peer: p.Host_id,
+	})
+	responseBody := bytes.NewBuffer(postBody)
+	//Leverage Go's HTTP Post function to make request
+	resp, err := http.Post("http://43.205.198.157:3000/advertise", "application/json", responseBody)
+	//Handle Error
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	fmt.Println(resp.StatusCode)
+}
+
+type sign_req_struct struct {
+	Group_pub_key string `json:"group_pub_key"`
+	Partial_key   string `json:"partial_key"`
+	Sign          string `json:"sign"`
+}  
+
+type signed_msgs struct {
+	Txn_id string `json:"txn_id"`
+	Msg    string `json:"msg"`
+	Nonce  int    `json:"nonce"`
+}
+
+func post_get_signatures( key *sign_req_struct) ( result []signed_msgs) { 
+	
+	url := "localhost:3000/post_signature"
+	fmt.Println("URL:>", url)
+
+	postBody, _ := json.Marshal(key)
+	responseBody := bytes.NewBuffer(postBody) 
+    
+	//Leverage Go's HTTP Post function to make request
+	resp, err := http.Post("localhost:3000/post_signature", "application/json", responseBody)
+	//Handle Error
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	fmt.Println(resp.StatusCode)
+
+	return result
+} 
